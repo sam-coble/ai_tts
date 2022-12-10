d6b80e812bee8f1bd32cd6e261cbf0f55e24ae33
sam <coblesam360@gmail.com>
2022-12-10 14:21:30 -0800

    play test audio (broken)

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log('test');
+const express = require('express');
+const path = require('path')
+const {readFile} = require('fs').promises;
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const app = express();
+app.use(express.static(path.join(__dirname, '/ai_tts')));
+app.use(cors());
+
+app.get('/', async (req, res) => {
+	res.status(200)
+		.header('Content-Type', 'text/html')
+		.send(await readFile('./index.html', 'utf8'));
+});
+app.get('/p5.sound.min.js', async (req, res) => {
+	res.status(200)
+		.header('Content-Type', 'text/javascript')
+		.send(await readFile('./assets/p5.sound.min.js', 'utf8'));
+});
+app.get('/p5.sound.min.js.map', async (req, res) => {
+	res.status(200)
+		.header('Content-Type', 'text/javascript')
+		.send(await readFile('./assets/p5.sound.min.js.map', 'utf8'));
+});
+app.get('/getAudio', async (req, res) => {
+	console.log('getting audio');
+	res.status(200)
+		.header('Content-Type', 'audio/wav')
+		.send(await readFile('./assets/test.wav'));
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`App Available`));
